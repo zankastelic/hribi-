4d4487aae84e6f6678145b70d581be1464939301
zankastelic <zkastelic15@gmail.com>
2019-12-10 00:34:29 +0100

    sem zgeneriral in zapisal linke gorovij

--- links.py
+++ links.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
-import requests 
+import requests
+import csv
 
 
 root_url = 'https://www.hribi.net'
@@ -11,7 +12,7 @@ with open('table.txt', 'r') as f:
         line = line.strip()
         filtered.append(line)
     
-    table = "".join(filtered) # zduzila sva vrstice datoteke v en string
+    table = "".join(filtered) # zduzitev vrstic v datoteki v en string
 
 tabela_gor = BeautifulSoup(table, 'html.parser')
 linki = tabela_gor.find_all('a')
@@ -21,39 +22,50 @@ generirani_linki = []
 for link in linki:
     link =  root_url + link['href']
     generirani_linki.append(link)
+print(generirani_linki)
 
-# print(generirani_linki)
 
-for link in generirani_linki:
-    content = requests.get(link).content
-    page = BeautifulSoup(content, 'html.parser')
-    vsebina_z_gorami = page.find(id='gorovjaseznam')
-    linki = vsebina_z_gorami.find_all('a')[3:]
+# podatki, ki ji bomo zapisali --> generirani linki: 
+outF = open("gorovja.txt","w")
+for link in generirani_linki: 
+    outF.write(link)
+    outF.write("\n")
+outF.close()
 
-    podlinki = []
 
-    for x in linki:
-        x =  root_url + x['href']
-        podlinki.append(x)
-    
 
-    for element in podlinki: 
-        content = requests.get(element).content
-        page = BeautifulSoup(content, 'html.parser')
-        podatki_vrha = page.find(id='vsebina')
-        info = podatki_vrha.find_all('b')
-        #for i in info:
-         #   print(i.parent.text.split(':'))
 
-        #print(podatki_vrha.prettify())
-        #opis = podatki_vrha.find_all('p')
-        #for o in opis:
-        #    print('boy')
-        #    print(o.parent)
-        poti = podatki_vrha.find_all('table')[2]
-        print(podatki_vrha)
-        break
+#for link in generirani_linki:
+#    content = requests.get(link).content
+#    page = BeautifulSoup(content, 'html.parser')
+#    vsebina_z_gorami = page.find(id='gorovjaseznam')
+#    linki = vsebina_z_gorami.find_all('a')[3:]
+#
+#    podlinki = []
+#
+#    for x in linki:
+#        x =  root_url + x['href']
+#        podlinki.append(x)
+#    
+#
+#    for element in podlinki: 
+#        content = requests.get(element).content
+#        page = BeautifulSoup(content, 'html.parser')
+#        podatki_vrha = page.find(id='vsebina')
+#        info = podatki_vrha.find_all('b')
+#        #for i in info:
+#         #   print(i.parent.text.split(':'))
+#
+#        #print(podatki_vrha.prettify())
+#        #opis = podatki_vrha.find_all('p')
+#        #for o in opis:
+#        #    print('boy')
+#        #    print(o.parent)
+#        poti = podatki_vrha.find_all('table')[2]
+#        print(podatki_vrha)
+#        break
+#
+#    break
 
-    break
 
 
