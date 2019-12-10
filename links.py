df4e3fd55da5b7d34f844d09114dfdd4b17f21db
zankastelic <zkastelic15@gmail.com>
2019-12-10 01:04:33 +0100

    se vedno generiram linke

--- links.py
+++ links.py
@@ -22,9 +22,9 @@ generirani_linki = []
 for link in linki:
     link =  root_url + link['href']
     generirani_linki.append(link)
-print(generirani_linki)
+#print(generirani_linki)
 
-
+# jih zapišemo 
 # podatki, ki ji bomo zapisali --> generirani linki: 
 outF = open("gorovja.txt","w")
 for link in generirani_linki: 
@@ -32,22 +32,25 @@ for link in generirani_linki:
     outF.write("\n")
 outF.close()
 
+for link in generirani_linki:
+    content = requests.get(link).content
+    page = BeautifulSoup(content, 'html.parser')
+    vsebina_z_gorami = page.find(id='gorovjaseznam')
+    linki = vsebina_z_gorami.find_all('a')[3:]
 
+    podlinki = []
 
+    for x in linki:
+        x =  root_url + x['href']
+        podlinki.append(x)
+print("dela")
 
-#for link in generirani_linki:
-#    content = requests.get(link).content
-#    page = BeautifulSoup(content, 'html.parser')
-#    vsebina_z_gorami = page.find(id='gorovjaseznam')
-#    linki = vsebina_z_gorami.find_all('a')[3:]
-#
-#    podlinki = []
-#
-#    for x in linki:
-#        x =  root_url + x['href']
-#        podlinki.append(x)
-#    
-#
+outV = open("vrhovi.txt","w")
+for x in podlinki: 
+    outV.write(x)
+    outV.write("\n")
+outV.close()
+
 #    for element in podlinki: 
 #        content = requests.get(element).content
 #        page = BeautifulSoup(content, 'html.parser')
