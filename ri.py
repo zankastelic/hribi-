6eec9ceb214f445846354399d72b19c22fade7da
zankastelic <zkastelic15@gmail.com>
2019-12-19 16:07:59 +0100

    asdfs

--- ri.py
+++ ri.py
@@ -12,7 +12,7 @@ import orodja
 #    vsebina = f.read()
 
 vzorec = (
-    r'<title>([\D|\s]+)<\/.*e>\s*'#ime
+    r'<title>(?P<ime>[\D|\s]+)<\/.*e>\s*'#ime
     r'[\s|\D|\d]*'
     r'<tr>.*[0-9]>(?P<drzava>\D*?)<\/a.*tr>\s*' #država
     r'<tr>.*">(?P<gorovje>\D*?)<.*<\/tr>\s*'  #gorovje
@@ -28,44 +28,24 @@ vzorec = (
     r'<tr.*"justify">(?P<opis>[\s|\D|\d]*)\s<\/p><\/td><\/tr>' #opis gore 
 )
 
-vzorec_1 = (r'<title>(?P<ime>.*?)</title>.*?<a class=moder href=.*?>(?P<drzava>.*?)</a>')
 
 # hribi = []
-# for zadetek in re.finditer(vzorec_1, vsebina):
-#     hribi.append(zadetek.groupdict())
+for zadetek in re.finditer(vzorec_1, vsebina):
+    hribi.append(zadetek.groupdict())
 
 
 
-# with open('hribi.json', 'w') as f:
-#     json.dump(hribi, f, indent=2)
-# f.close()
+with open('hribi.json', 'w') as f:
+    json.dump(hribi, f, indent=2)
+    f.close()
 
-# with open('hribi.csv', 'w', encoding='utf-8') as csv:
-#     #writer = csv.DictWriter(csv, fieldnames=['ime', 'država', 'gorovje', 'višina', 'vrsta', 'ogledi', 'priljubljenost_[%]', 'priljubljenost_[mesto]', 'število_poti', 'opis_gore'])
-#     writer = csv.DictWriter(csv, fieldnames=['ime', 'država'])
-#     writer.writeheader()
-#     for x in hribi:
-#         writer.writerow(x)
-# csv.close() 
+with open('hribi.csv', 'w', encoding='utf-8') as csv:
+    writer = csv.DictWriter(csv, fieldnames=['ime', 'država', 'gorovje', 'višina', 'vrsta', 'ogledi', 'priljubljenost_[%]', 'priljubljenost_[mesto]', 'število_poti', 'opis_gore'])
+    writer = csv.DictWriter(csv, fieldnames=['ime', 'država'])
+    writer.writeheader()
+    for x in hribi:
+        writer.writerow(x)
+csv.close() 
 
-ime_datoteke = 'vsebina_spletnih_strani.html'
 
-def pridobi_podatke_o_oglasih(ime_datoteke):
-    #seznam_oglasov = posamezni_oglasi(ime_datoteke)  #seznam po oglasih
-    slovar_iskanih_podatkov = []
-    stevec = 0
-    n = 100
-    while n > 0:
-        for zadetek in re.finditer(vzorec_1, ime_datoteke):
-            slovar_zadetkov = zadetek.groupdict()
-            slovar_iskanih_podatkov.append(slovar_zadetkov)
-            stevec += 1
-            n -= 1
-            #print(stevec)
-    print(stevec, slovar_iskanih_podatkov)
-
-    orodja.zapisi_csv(slovar_iskanih_podatkov, ['naslov', 'podnaslov'], 'hribi1.csv')
-    print('končano:', len(slovar_iskanih_podatkov))
-
-    orodja.zapisi_json(slovar_iskanih_podatkov, 'hribi3.json')
 
